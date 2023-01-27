7ef0ac2afd56dc9aec1d4c3217be01f7c5c4f9a5
Ekaterina <katya_usova@bk.ru>
2023-01-27 08:36:11 +0300

    HomeWork27

---
+++ HomeWork27/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ekaterina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("A = ");
+            int A = System.Convert.ToInt32(System.Console.ReadLine());
+            count(A);
+            System.Console.ReadKey(true);
+        }
+
+        static void count(int A)
+        {
+            int res = 0;
+            int A1 = A;
+            while (A > 0)
+            {
+                int B = A % 10;
+                res+=B;
+                A /= 10;
+            }
+            System.Console.Write("{0} -> {1}", A1, res);
+        }
+    }
+}
