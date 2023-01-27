1c8be9306c07338b0bcf1b58e9fa768ea8c51115
Ekaterina <katya_usova@bk.ru>
2023-01-27 08:40:43 +0300

    HomeWork29

---
+++ HomeWork29/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ekaterina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[8];
+            arr(array);
+            System.Console.ReadKey(true);
+        }
+        static void arr(int[] array)
+        {
+            System.Random myRandom = new System.Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = myRandom.Next(0, 100);
+                System.Console.Write("{0}, ", array[i]);
+            }
+        }
+    }
+}
